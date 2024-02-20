c58d36fa2eafcd5c88094e2f3c39be8631b375db
Thiago Yukio <thiago_pacheco@edu.univali.br>
2024-02-20 15:25:08 -0300

    Adicionado opção de ver distriubuição por sexo do tubarão

--- codigo/grafico_teste_SliderInputANOS.R
+++ codigo/grafico_teste_SliderInputANOS.R
@@ -6,13 +6,17 @@ gerar_dados_tubaroes <- function(media, desvio_padrao, n_amostras) {
   tamanhos <- rnorm(n_amostras, mean = media, sd = desvio_padrao)
   tamanhos <- pmax(tamanhos, 1)  # Garante que não haja tamanhos abaixo de 1
   tamanhos <- pmin(tamanhos, 300)  # Garante que não haja tamanhos acima de 300
-  data.frame(Tamanho = tamanhos)
+  sexo <- sample(c("M", "F"), size = n_amostras, replace = TRUE)
+  data.frame(Tamanho = tamanhos, Sexo = sexo)
 }
 
 ui <- fluidPage(
   sliderInput("intervalo_anos", "Intervalo de Anos", min = 2018, max = 2023,
               value = c(2018, 2023), step = 1),
   verbatimTextOutput("anos_selecionados"),
+  radioButtons("sexo_escolhido", "Escolha o Sexo:",
+               choices = c("Todos", "Macho", "Fêmea"),
+               selected = "Todos"),
   plotOutput("grafico")
 )
 
@@ -31,7 +35,17 @@ server <- function(input, output, session) {
   })
   
   output$grafico <- renderPlot({
-    hist_rel <- hist(dados_tubaroes()$Tamanho, plot = FALSE)
+    dados <- dados_tubaroes()
+    
+    # Filtra os dados com base na escolha do sexo
+    if (input$sexo_escolhido == "Macho") {
+      dados <- subset(dados, Sexo == "M")
+    } else if (input$sexo_escolhido == "Fêmea") {
+      dados <- subset(dados, Sexo == "F")
+    }
+    
+    # Cria o histograma e o gráfico de barras
+    hist_rel <- hist(dados$Tamanho, plot = FALSE)
     frequencia_relativa <- hist_rel$counts / sum(hist_rel$counts) * 100
     barplot(frequencia_relativa, names.arg = hist_rel$mids,
             main = "Histograma de Frequência Relativa",
