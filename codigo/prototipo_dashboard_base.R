90208acdef60e2bd8f7ce7f7c731281dd3d2924b
Thiago Yukio <thiago_pacheco@edu.univali.br>
2024-03-11 15:38:35 -0300

    Ajuste em leia me e o funcionamento do accordion com o dashboard body, porém ainda falta conseguir imprimir uma imagem

--- codigo/prototipo_dashboard_base.R
+++ codigo/prototipo_dashboard_base.R
@@ -1,33 +1,31 @@
-  if(interactive()){
-    
-    options(device.ask.default = FALSE)
-    
-    if(!require(pacman)) {
-      print(paste0("Pacote ainda não instalado. Irei instalar agora!"))
-      install.packages("pacman", dependencies = TRUE)
-      library(pacman)
-    } else {
-      print(paste0("Pacote já estava instalado e já carregado para o trabalho"))
-      library(pacman)
-      }
-    
-    p_load(shiny, shinydashboard, shinydashboardPlus, shinythemes)
-    
-    shinyApp(
-      ui = dashboardPage(
-        options = list(sidebarExpandOnHover = TRUE),
-        header = dashboardHeader(
-           title = "  Projeto Tubarão Azul"
-           ),
-        # ),
-        sidebar = dashboardSidebar(width = 250,
-          
-          id = "sidebar",
-          minified = FALSE,
-          collapsed = FALSE,
-          
-          tags$head(
-            tags$style(HTML('
+if(interactive()){
+  
+  options(device.ask.default = FALSE)
+  
+  if(!require(pacman)) {
+    print(paste0("Pacote ainda não instalado. Irei instalar agora!"))
+    install.packages("pacman", dependencies = TRUE)
+    library(pacman)
+  } else {
+    print(paste0("Pacote já estava instalado e já carregado para o trabalho"))
+    library(pacman)
+  }
+  
+  p_load(shiny, shinydashboard, shinydashboardPlus, shinythemes, readtext)
+  
+  shinyApp(
+    ui = dashboardPage(
+      options = list(sidebarExpandOnHover = TRUE),
+      header = dashboardHeader(
+        title = "  Projeto Tubarão Azul"
+      ),
+      sidebar = dashboardSidebar(
+        width = 250,
+        id = "sidebar",
+        minified = FALSE,
+        collapsed = FALSE,
+        tags$head(
+        tags$style(HTML('
             /* Ajuste o tamanho dos títulos das abas dentro do sidebarPanel */
             .custom-sidebar .nav-tabs > li > a {
               width: 240px; /* Largura fixa para os títulos das abas */
@@ -40,138 +38,183 @@
               color: #000000 !important; /* Cor preta */
             }
           '))
-          ),
-          
-      #     tags$head(
-      #       tags$style(HTML("
-      #   /* Estilo para alterar a cor do texto do sidebar para preto */
-      #   #sidebar .sidebar-menu li a,
-      #   #sidebar .sidebar-menu li a:hover,
-      #   #sidebar .sidebar-menu label {
-      #     color: #000000 !important; /* Cor preta */
-      #   }
-      #   #sidebar .sidebar-menu span {
-      #     color: #000000 !important; /* Cor preta */
-      #   }
-      #   #sidebar .sidebar-menu p {
-      #     color: #000000 !important; /* Cor preta */
-      #   }
-      # "))
-      #     ),
-          sidebarMenu(
-            id = "sidebarMenu",
-            sidebarLayout(
-              sidebarPanel(width = 250,class = "custom-sidebar",
-               tabsetPanel(
-                 id = "tabs",
-                 tabPanel("Apresentação", value = "tab1"),
-                 tabPanel("Distribuição de comprimentos", value = "tab2"),
-                 tabPanel("Desembarques", value = "tab3"),
-                 tabPanel("Distribuição espacial das capturas", value = "tab4")
-               )
+        ),
+       #     tags$head(
+       #       tags$style(HTML("
+       #   /* Estilo para alterar a cor do texto do sidebar para preto */
+       #   #sidebar .sidebar-menu li a,
+       #   #sidebar .sidebar-menu li a:hover,
+       #   #sidebar .sidebar-menu label {
+       #     color: #000000 !important; /* Cor preta */
+       #   }
+       #   #sidebar .sidebar-menu span {
+       #     color: #000000 !important; /* Cor preta */
+       #   }
+       #   #sidebar .sidebar-menu p {
+       #     color: #000000 !important; /* Cor preta */
+       #   }
+       # "))
+       #     ),
+        sidebarMenu(
+          id = "sidebarMenu",
+          sidebarLayout(
+            sidebarPanel(
+              width = 250,class = "custom-sidebar",
+              tabsetPanel(
+                id = "headerTab",
+                tabPanel("Apresentação", value = "tab1header"),
+                tabPanel("Distribuição de comprimentos", value = "tab2header"),
+                tabPanel("Desembarques", value = "tab3header"),
+                tabPanel("Distribuição espacial das capturas", value = "tab4header")
+                )
               ),
-              mainPanel(
-                img(src = 'dados_brutos/ImagemSide.png', height = "100%", width = "100%")
+            mainPanel(
+              imageOutput(outputId = "imgSide",width = "50%", height = "50%")
               )
             )
           )
         ),
-        body = dashboardBody(
-          tags$head(
-            tags$style(HTML('
+      body = dashboardBody(
+        tabsetPanel(
+          id = "bodyTab",
+          tabPanel("Projeto", value = "tab1body"),
+          tabPanel("Leia-me", value = "tab2body")
+        ),
+        tags$head(
+          tags$style(HTML('
             /* Definir a largura do accordion */
             #accordion .box {
               width: 520px; /* Substitua por qualquer valor que desejar */
               text-align: center; /* Centraliza o texto */
             }
           '))
-          ),
-          uiOutput("accordion_ui")
-          ),
-        controlbar = dashboardControlbar(
-          collapsed = FALSE,
-          id = "controlbar",
-          controlbarMenu(
-            id = "controlbarMenu",
-            
-            controlbarItem(
-              "Opções",
-              sliderInput(
-                "obs",
-                "Number of observations:",
-                min = 0,
-                max = 1000,
-                value = 10,
-                step = 10,
-                animate = animationOptions(
-                  interval = 800,
-                  playButton = icon("play"),
-                  pauseButton = icon("pause")
-                )
-              ),
-              checkboxGroupInput(
-                "species", "Seletor de Espécies:",
-                c("Albacora bandolim","Albacora branca","Albacora laje",
-                  "Meca", "Outros"),
-                selected = c("Albacora bandolim","Albacora branca",
-                             "Albacora laje", "Meca","Outros")
-              ),
-            ),
-            
-            controlbarItem(
-              "Tema",
-              "Bem-Vindo ao Seletor de Tema",
-              skinSelector()
-              )
-          )
-          
         ),
-        title = "Teste ShinyDashboardPlus",
+        uiOutput("accordion_ui")
       ),
-      server <- function(input, output, session) {
-        output$accordion_ui <- renderUI({
-          if(input$tabs == "tab1") {
-            accordion(
-              id = "accordion",
-              width = 60,
-              accordionItem(
-                title = "Visualização dos Dados",
-                status = "primary",
-                collapsed = FALSE,
-                plotOutput("distPlot", width = "500px", height = "500px")
+      controlbar = dashboardControlbar(
+        collapsed = FALSE,
+        id = "controlbar",
+        controlbarMenu(
+          id = "controlbarMenu",
+          controlbarItem(
+            "Opções",
+            sliderInput(
+              "obs",
+              "Number of observations:",
+              min = 0,
+              max = 1000,
+              value = 10,
+              step = 10,
+              animate = animationOptions(
+                interval = 800,
+                playButton = icon("play"),
+                pauseButton = icon("pause")
+              )
+            ),
+            checkboxGroupInput(
+              "species", "Seletor de Espécies:",
+              c("Albacora bandolim","Albacora branca","Albacora laje",
+                "Meca", "Outros"),
+              selected = c("Albacora bandolim","Albacora branca",
+                           "Albacora laje", "Meca","Outros")
+            ),
+          ),
+          
+          controlbarItem(
+            "Tema",
+            "Bem-Vindo ao Seletor de Tema",
+            skinSelector()
+          )
+        )
+        
+      ),
+      title = "Teste ShinyDashboardPlus",
+    ),
+    server <- function(input, output, session) {
+      
+      docx_content1 <- readtext("dados_brutos/testeWord.docx")
+      
+      docx_content2 <- readtext("dados_brutos/leiame.docx")
+      
+      output$accordion_ui <- renderUI({
+        if(input$headerTab == "tab2header") {
+          accordion(
+            id = "accordion1",
+            width = 60,
+            accordionItem(
+              title = "Visualização dos Dados",
+              status = "primary",
+              collapsed = FALSE,
+              fluidRow(
+                column(6, plotOutput("distPlot")),
+                column(6, plotOutput("plot2")),
+                column(6, plotOutput("plot3")),
+                column(6, plotOutput("plot4")),
               )
             )
-          } else if(input$tabs == "tab2") {
-            accordion(
-              id = "accordion",
-              width = 60,
-              accordionItem(
-                title = "Visualização de Texto",
-                status = "success",
-                collapsed = FALSE,
-                textOutput("textOut")
-              )
+          )
+        } else if(input$headerTab == "tab1header") {
+          accordion(
+            id = "accordion2",
+            width = 60,
+            accordionItem(
+              title = "Visualização de Texto",
+              status = "primary",
+              collapsed = FALSE,
+              # textOutput("textOut1"),
+              # textOutput("textOut2")
+              if(input$bodyTab == "tab1body"){
+                textOutput("textOut1")
+              } else{
+                textOutput("textOut2")
+              }
             )
-          }
-        })
-        
-        output$distPlot <- renderPlot({
-          if(input$obs != 0 && input$tabs == "tab1"){
-            hist(rnorm(input$obs), col = "blue",
-                 main = paste("Histograma Teste - Observações:", input$obs),
-                 xlab = "", freq = TRUE)
-          }
-        })
-        
-        output$textOut <- renderText({
-          if(input$tabs == "tab2"){
-            texto <- readLines("dados_brutos/TesteOutputText.txt", warn = FALSE)
-            paste(texto, collapse = "\n")
-          }
-        })
+          )
+        }
+      })
+      
+      # output$textOut <- renderText({
+      #   if(input$tabs == "tab1header"){
+      #     texto <- readLines("dados_brutos/testeOutputText.txt", warn = FALSE)
+      #     paste(texto, collapse = "\n")
+      #   }
+      # })
+      
+      output$textOut1 <- renderText({
+        if(input$headerTab == "tab1header" && input$bodyTab == "tab1body"){
+          paste(docx_content1$text, collapse = "\n")
+        }
+      })
+      
+      output$textOut2 <- renderText({
+        if(input$headerTab == "tab1header" && input$bodyTab == "tab2body"){
+          paste(docx_content2$text, collapse = "\n")
+        }
+      })
+      
+      output$distPlot <- renderPlot({
+        if(input$obs != 0 && input$headerTab == "tab2header"){
+          hist(rnorm(input$obs), col = "blue",
+               main = paste("Histograma Teste - Observações:", input$obs),
+               xlab = "", freq = TRUE)
+        }
+      })
+      
+      output$plot2 <- renderPlot({
+        if(input$obs != 0 && input$headerTab == "tab2header"){
+         plot(density(rnorm(input$obs)))
+        }
+      })
+      
+      output$plot3 <- renderPlot({
+        data <- data.frame(Label = c("A", "B", "C"), Value = c(30, 40, 20))
+        pie(data$Value, labels = data$Label, col = rainbow(nrow(data)))
+      })
+      
+      output$plot4 <- renderPlot({
+        barplot(table(sample(letters[1:5], input$obs, replace = TRUE)))
+      })
+      
       }
-      
-      
     )
-  }
-  
+}
