1a8d29aaf3166f456dea6f673d5c0a07ab5d4e0e
Thiago Yukio <thiago_pacheco@edu.univali.br>
2024-03-14 14:35:27 -0300

    Ajuste para funcionamento de gráficos, criados anteriormente em shinydashboard, e agora movidos para shinydashboardPlus

--- codigo/grafico_teste_SliderInputANOS.R
+++ codigo/grafico_teste_SliderInputANOS.R
@@ -3,93 +3,99 @@ library(shinyFeedback)
 library(scales)
 library(ggplot2)
 library(dplyr)
+library(shinydashboard)
+library(shinydashboardPlus)
 
-gerar_dados_tubaroes <- function(media, desvio_padrao, n_amostras, ano_inicial, ano_final) {
-  tamanhos <- rnorm(n_amostras, mean = media, sd = desvio_padrao)
-  tamanhos <- pmax(tamanhos, 1)  # Garante que não haja tamanhos abaixo de 1
-  tamanhos <- pmin(tamanhos, 300)  # Garante que não haja tamanhos acima de 300
-  sexo <- sample(c("M", "F"), size = n_amostras, replace = TRUE)
-  anos <- sample(seq(ano_inicial, ano_final), size = n_amostras, replace = TRUE)
-  data.frame(Tamanho = tamanhos, Sexo = sexo, Ano = anos)
-}
 
-ui <- fluidPage(
-  fluidRow(
-    column(3,
-           sliderInput("intervalo_anos", "Intervalo de Anos", min = 2018, max = 2023,
-                       value = c(2018, 2023), step = 1),
-           #verbatimTextOutput("anos_selecionados"),
-           radioButtons("sexo_escolhido", "Escolha o Sexo:",
-                        choices = c("Todos", "Macho", "Fêmea"),
-                        selected = "Todos")
+
+shinyApp(
+  ui = dashboardPage(
+    options = list(sidebarExpandOnHover = TRUE),
+    header = dashboardHeader(
+      title = "Projeto Tubarão Azul"
+      ),
+    sidebar = dashboardSidebar(
+      sidebarMenu(
+        sidebarLayout(
+          sidebarPanel(),
+          mainPanel()
+        )
+      )
     ),
-    column(9,
-           plotOutput("grafico1"),
-           plotOutput("grafico2")
-    )
+    body = dashboardBody(
+      plotOutput("grafico1"),
+      plotOutput("grafico2")
+      )
+    ),
+    controlbar = dashboardControlbar(
+      sliderInput(
+        "intervalo_anos", 
+        "Intervalo de Anos",
+        min = 2018, 
+        max = 2023,
+        value = c(2018, 2023),
+        step = 1
+        ),
+      radioButtons(
+        "sexo_escolhido", 
+        "Escolha o Sexo:",
+        choices = c("Todos", "Macho", "Fêmea"),
+        selected = "Todos"
+        )
+      ),
+      server <- function(input, output, session) {
+        # dados_tubaroes <- reactive({
+        #   #intervalo <- input$intervalo_anos[1]:input$intervalo_anos[2]
+        #   dados_teste
+        #   #gerar_dados_tubaroes(150, 10, 1000, 2018, 2023)  # Média de 150, desvio padrão de 30
+        # })
+        # 
+        # # output$anos_selecionados <- renderText({
+        # #   intervalo <- input$intervalo_anos
+        # #   paste("Intervalo de anos selecionados: ", paste(intervalo, collapse = "-"))
+        # # })
+        # 
+        # output$grafico1 <- renderPlot({
+        #   dados <- dados_tubaroes()
+        #   
+        #   dados <- subset(dados, Ano >= input$intervalo_anos[1]
+        #                   & Ano <= input$intervalo_anos[2])
+        #   
+        #   # Filtra os dados com base na escolha do sexo
+        #   if (input$sexo_escolhido == "Macho") {
+        #     dados <- subset(dados, Sexo == "M")
+        #   } else if (input$sexo_escolhido == "Fêmea") {
+        #     dados <- subset(dados, Sexo == "F")
+        #   }
+        #   
+        #   # Cria o gráfico ggplot
+        #   ggplot(dados, aes(x = Tamanho)) +
+        #     geom_histogram(binwidth = 10, fill = "blue", color = "black") +
+        #     labs(title = "Distribuição de Tamanhos de Tubarões",
+        #          x = "Tamanho (cm)",
+        #          y = "Frequência (%)") +
+        #     scale_y_continuous(labels = scales::label_percent(scale = 0.01))
+        # })
+        # 
+        # output$grafico2 <-renderPlot({
+        #   dados <- dados_tubaroes()
+        #   
+        #   dados <- subset(dados, Ano >= input$intervalo_anos[1] & Ano <= input$intervalo_anos[2])
+        #   
+        #   gender <- dados_teste %>%
+        #     count(Sexo)
+        #   
+        #   gender <- gender %>%
+        #     mutate(porc = n / sum(n) * 100)
+        #   
+        #   gender$ymax <- cumsum(gender$porc)
+        #   gender$ymin <- c(0, head(gender$ymax, n=1))
+        #   
+        #   ggplot(gender, aes(ymax=ymax, ymin=ymin, xmax = 4, xmin = 3, fill = Sexo)) +
+        #     geom_rect() +
+        #     coord_polar(theta = "y") +
+        #     xlim(c(1,4)) +
+        #     theme_void()
+        # })
+        }
   )
-)
-
-server <- function(input, output, session) {
-  
-  # Gera dados de tamanhos de tubarões com base nos parâmetros escolhidos
-  dados_tubaroes <- reactive({
-    #intervalo <- input$intervalo_anos[1]:input$intervalo_anos[2]
-    dados_teste
-    #gerar_dados_tubaroes(150, 10, 1000, 2018, 2023)  # Média de 150, desvio padrão de 30
-  })
-  
-  # output$anos_selecionados <- renderText({
-  #   intervalo <- input$intervalo_anos
-  #   paste("Intervalo de anos selecionados: ", paste(intervalo, collapse = "-"))
-  # })
-  
-  output$grafico1 <- renderPlot({
-    dados <- dados_tubaroes()
-    
-    dados <- subset(dados, Ano >= input$intervalo_anos[1] 
-                    & Ano <= input$intervalo_anos[2])
-    
-    # Filtra os dados com base na escolha do sexo
-    if (input$sexo_escolhido == "Macho") {
-      dados <- subset(dados, Sexo == "M")
-    } else if (input$sexo_escolhido == "Fêmea") {
-      dados <- subset(dados, Sexo == "F")
-    }
-    
-    # Cria o gráfico ggplot
-    ggplot(dados, aes(x = Tamanho)) +
-      geom_histogram(binwidth = 10, fill = "blue", color = "black") +
-      labs(title = "Distribuição de Tamanhos de Tubarões",
-           x = "Tamanho (cm)",
-           y = "Frequência (%)") +
-      scale_y_continuous(labels = scales::label_percent(scale = 0.01))
-  })
-  
-  output$grafico2 <-renderPlot({
-    dados <- dados_tubaroes()
-    
-    dados <- subset(dados, Ano >= input$intervalo_anos[1] & Ano <= input$intervalo_anos[2])
-
-    gender <- dados_teste %>%
-      count(Sexo)
-    
-    gender <- gender %>%
-      mutate(porc = n / sum(n) * 100)
-    
-    gender$ymax <- cumsum(gender$porc)
-    gender$ymin <- c(0, head(gender$ymax, n=1))
-    
-    ggplot(gender, aes(ymax=ymax, ymin=ymin, xmax = 4, xmin = 3, fill = Sexo)) +
-      geom_rect() +
-      coord_polar(theta = "y") +
-      xlim(c(1,4)) +
-      theme_void()
-  })
-}
-
-shinyApp(ui, server)
-
-
-
-  
--- codigo/prototipo_dashboard_base.R
+++ codigo/prototipo_dashboard_base.R
@@ -1,34 +1,24 @@
-# dados <- read.table("dados_brutos/dados_brutos.csv",header = T, sep = ";", dec = ",")
+# Pacotes necessários
+if (!require("pacman")) {
+  install.packages("pacman")
+}
+pacman::p_load(
+  shiny, shinydashboard, shinydashboardPlus, shinythemes,
+  readtext, dplyr, ggplot2, ggrepel, scales
+)
 
-if(interactive()){
-  
-  options(device.ask.default = FALSE)
-  
-  if(!require(pacman)) {
-    print(paste0("Pacote ainda não instalado. Irei instalar agora!"))
-    install.packages("pacman", dependencies = TRUE)
-    library(pacman)
-  } else {
-    print(paste0("Pacote já estava instalado e já carregado para o trabalho"))
-    library(pacman)
-  }
-  
-  p_load(shiny, shinydashboard, shinydashboardPlus, shinythemes, 
-         readtext, dplyr, ggplot2, ggrepel, scales)
-  
-  shinyApp(
-    ui = dashboardPage(
-      options = list(sidebarExpandOnHover = TRUE),
-      header = dashboardHeader(
-        title = "  Projeto Tubarão Azul"
-      ),
-      sidebar = dashboardSidebar(
-        width = 250,
-        id = "sidebar",
-        minified = FALSE,
-        collapsed = F,
-        tags$head(
-          tags$style(HTML('
+# Função principal ShinyApp
+shinyApp(
+  ui = dashboardPage(
+    # Definições de layout
+    header = dashboardHeader(title = "Projeto Tubarão Azul"),
+    sidebar =  dashboardSidebar(
+      width = 250,
+      id = "sidebar",
+      minified = FALSE,
+      collapsed = FALSE,
+      tags$head(
+        tags$style(HTML('
           /* Ajuste o tamanho dos títulos das abas dentro do sidebarPanel */
           .custom-sidebar .nav-tabs > li > a {
             width: 240px; /* Largura fixa para os títulos das abas */
@@ -41,138 +31,116 @@ if(interactive()){
             color: #000000 !important; /* Cor preta */
           }
           .texto-accordion {
-          display: inline-block;
-          margin: 10px auto;
+            display: inline-block;
+            margin: 10px auto;
           }
-      
           .texto-accordion .accordion-title {
-          text-align: center; /* Centraliza o texto apenas nos títulos */
+            text-align: center; /* Centraliza o texto apenas nos títulos */
           }
-          
           .graficos-accordion {
-          margin: 0 auto;
+            margin: 0 auto;
           }
-
         '))
-        ),
-       #     tags$head(
-       #       tags$style(HTML("
-       #   /* Estilo para alterar a cor do texto do sidebar para preto */
-       #   #sidebar .sidebar-menu li a,
-       #   #sidebar .sidebar-menu li a:hover,
-       #   #sidebar .sidebar-menu label {
-       #     color: #000000 !important; /* Cor preta */
-       #   }
-       #   #sidebar .sidebar-menu span {
-       #     color: #000000 !important; /* Cor preta */
-       #   }
-       #   #sidebar .sidebar-menu p {
-       #     color: #000000 !important; /* Cor preta */
-       #   }
-       # "))
-       #     ),
-        sidebarMenu(
-          id = "sidebarMenu",
-          sidebarLayout(
-            sidebarPanel(
-              width = 250,class = "custom-sidebar",
-              tabsetPanel(
-                id = "headerTab",
-                tabPanel("Apresentação", value = "tab1header"),
-                tabPanel("Distribuição de comprimentos", value = "tab2header"),
-                tabPanel("Desembarques", value = "tab3header"),
-                tabPanel("Distribuição espacial das capturas", value = "tab4header")
-                ),
-              imageOutput("creditos_img")
-              ),
-            mainPanel()
-            )
-          )
-        ),
-      body = dashboardBody(
-        uiOutput("tabset_ui"),
-        fluidRow(
-          column(9,
-                 uiOutput("accordion_ui")
-          )#,
-          # column(3,
-          #        imageOutput("creditos_img")
-          #        )
-          )
-        # uiOutput("accordion_ui"),
-        # uiOutput("creditos_ui")
       ),
-      controlbar = dashboardControlbar(
-        collapsed = T,
-        id = "controlbar",
-        controlbarMenu(
-          id = "controlbarMenu",
-          controlbarItem(
-            "Opções",
-            sliderInput(
-              "obs",
-              "Number of observations:",
-              min = 2018,
-              max = 2023,
-              value = c(2018,2023),
-              step = 1,
-              animate = animationOptions(
-                interval = 800,
-                playButton = icon("play"),
-                pauseButton = icon("pause")
-              )
+      sidebarMenu(
+        id = "sidebarMenu",
+        sidebarLayout(
+          sidebarPanel(
+            width = 250,class = "custom-sidebar",
+            tabsetPanel(
+              id = "headerTab",
+              tabPanel("Apresentação", value = "tab1header"),
+              tabPanel("Distribuição de comprimentos", value = "tab2header"),
+              tabPanel("Desembarques", value = "tab3header"),
+              tabPanel("Distribuição espacial das capturas", value = "tab4header")
             ),
-            checkboxGroupInput(
-              "species", "Seletor de Espécies:",
-              c("Albacora bandolim","Albacora branca","Albacora laje",
-                "Meca", "Outros"),
-              selected = c("Albacora bandolim","Albacora branca",
-                           "Albacora laje", "Meca","Outros")
-            ),
+            imageOutput("creditos_img")
           ),
-          controlbarItem(
-            "Tema",
-            "Bem-Vindo ao Seletor de Tema",
-            skinSelector()
+          mainPanel()
+        )
+      )
+    ),
+    body = dashboardBody(
+      uiOutput("tabset_ui"),
+      fluidRow(
+        column(9, uiOutput("accordion_ui"))
+      )
+    ),
+    controlbar = dashboardControlbar(
+      collapsed = TRUE,
+      id = "controlbar",
+      controlbarMenu(
+        id = "controlbarMenu",
+        controlbarItem(
+          "Opções",
+          sliderInput(
+            "intervalo_anos",
+            "Intervalo de Anos:",
+            min = 2018,
+            max = 2023,
+            value = c(2018,2023),
+            step = 1,
+            animate = animationOptions(
+              interval = 800,
+              playButton = icon("play"),
+              pauseButton = icon("pause")
             )
+          ),
+          radioButtons(
+            "sexo_escolhido", 
+            "Escolha o Sexo:",
+            choices = c("Todos", "Macho", "Fêmea"),
+            selected = "Todos"
+          ),
+          checkboxGroupInput(
+            "species", "Seletor de Espécies:",
+            c("Albacora bandolim","Albacora branca","Albacora laje",
+              "Meca", "Outros"),
+            selected = c("Albacora bandolim","Albacora branca",
+                         "Albacora laje", "Meca","Outros")
           )
         ),
-      title = "Teste ShinyDashboardPlus",
+        controlbarItem(
+          "Tema",
+          "Bem-Vindo ao Seletor de Tema",
+          skinSelector()
+        )
+      )
     ),
-    server <- function(input, output, session) {
-      
-      dados_tubaroes <- reactive({
-        # dados <- read.table("dados_brutos/dados_brutos.csv",header = T, sep = ";", dec = ",")
-        dados
-      })
-      
-      
-      
-      
-      pdf_content1 <- readtext("dados_brutos/testepdf.pdf")
-      
-      pdf_content2 <- readtext("dados_brutos/leiame.pdf")
-      
-      output$tabset_ui <- renderUI({
-        if(input$headerTab == "tab1header") {
-          tabsetPanel(
-            id = "bodyTab",
-            tabPanel("Projeto", value = "tab1body"),
-            tabPanel("Leia-me", value = "tab2body")
-          )
-        }
-      })
-      
-      output$creditos_img <- renderImage({
-        list(src = "dados_brutos/ImagemTeste.png",  # Substitua pelo caminho da sua imagem PNG
-             contentType = "image/png",
-             # width = 300,  # Largura da imagem
-             # height = 440,  # Altura da imagem
-          
-             alt = "Créditos")  # Texto alternativo para acessibilidade
-      }, deleteFile = FALSE)
-      
-      output$accordion_ui <- renderUI({
+    title = "Teste ShinyDashboardPlus"
+  ),
+  
+  server = function(input, output, session) {
+    # Leitura dos dados
+    dados_tubaroes <- reactive({
+      read.table("dados_brutos/dados_brutos.csv",
+                 header = TRUE, sep = ";", dec = ",")
+    })
+    
+    # Leitura dos arquivos PDF
+    pdf_content1 <- readtext("dados_brutos/testepdf.pdf")
+    pdf_content2 <- readtext("dados_brutos/leiame.pdf")
+    
+    # Geração dinâmica dos painéis de abas
+    output$tabset_ui <- renderUI({
+      if(input$headerTab == "tab1header") {
+        tabsetPanel(
+          id = "bodyTab",
+          tabPanel("Projeto", value = "tab1body"),
+          tabPanel("Leia-me", value = "tab2body")
+        )
+      }
+    })
+    
+    # Renderização da imagem
+    output$creditos_img <- renderImage({
+      list(src = "dados_brutos/ImagemTeste.png",  # Substitua pelo caminho da sua imagem PNG
+           contentType = "image/png",
+           alt = "Créditos")  # Texto alternativo para acessibilidade
+    }, deleteFile = FALSE)
+    
+    # Geração dinâmica dos accordions
+    output$accordion_ui <- renderUI({
       if(input$headerTab == "tab2header") {
         div(class = "graficos-accordion",
             accordion(
@@ -182,114 +150,87 @@ if(interactive()){
                 status = "primary",
                 collapsed = FALSE,
                 fluidRow(
-                  # column(6, plotOutput("graficoBarra")),
-                  # column(6, plotOutput("graficoRosca")),
-                  # column(6, plotOutput("plot3")),
-                  # column(6, plotOutput("plot4"))
+                  column(6, plotOutput("graficoBarra")),
+                  column(6, plotOutput("graficoRosca"))
                 )
               )
             )
-          )
-          
+        )
       } else if(input$headerTab == "tab1header") {
         div(class = "texto-accordion",
-          accordion(
-            id = "accordion2",
-            accordionItem(
-              title = "Visualização de Texto",
-              status = "primary",
-              collapsed = FALSE,
-              if(input$bodyTab == "tab1body"){
-                uiOutput("textOut1")
-              } else{
-                uiOutput("textOut2")
+            accordion(
+              id = "accordion2",
+              accordionItem(
+                title = "Visualização de Texto",
+                status = "primary",
+                collapsed = FALSE,
+                if(input$bodyTab == "tab1body") {
+                  uiOutput("textOut1")
+                } else {
+                  uiOutput("textOut2")
                 }
               )
             )
-          )
-        }
-      })
-      
-      output$graficoBarra <- renderPlot({
-        dadostub <- dados_tubaroes()
-
-        dadostub <- subset(dadostub, Ano >= input$intervalo_anos[1]
-                        & Ano <= input$intervalo_anos[2])
-
-        # Filtra os dados com base na escolha do sexo
-        if (input$sexo_escolhido == "Macho") {
-          dadostub <- subset(dadostub, Sexo == "M")
-        } else if (input$sexo_escolhido == "Fêmea") {
-          dadostub <- subset(dadostub, Sexo == "F")
-        }
-
-        # Cria o gráfico ggplot
-        ggplot(dadostub, aes(x = Tamanho)) +
-          geom_histogram(binwidth = 10, fill = "blue", color = "black") +
-          labs(title = "Distribuição de Tamanhos de Tubarões",
-               x = "Tamanho (cm)",
-               y = "Frequência (%)") +
-          scale_y_continuous(labels = scales::label_percent(scale = 0.01))
-      })
-      
-      output$graficoRosca <-renderPlot({
-        dadostub <- dados_tubaroes()
-        
-        dadostub <- subset(
-          dadostub, 
-          Ano >= input$intervalo_anos[1] & Ano <= input$intervalo_anos[2])
-        
-        gender <- dadostub %>%
-          count(Sexo)
-        
-        gender <- gender %>%
-          mutate(porc = n / sum(n) * 100)
-        
-        gender$ymax <- cumsum(gender$porc)
-        gender$ymin <- c(0, head(gender$ymax, n=1))
-        
-        ggplot(gender, aes(ymax=ymax, ymin=ymin, xmax = 4, xmin = 3, fill = Sexo)) +
-          geom_rect() +
-          coord_polar(theta = "y") +
-          xlim(c(1,4)) +
-          theme_void()
-      })
-      
-      output$textOut1 <- renderUI({
-        if(input$headerTab == "tab1header" && input$bodyTab == "tab1body") {
-          HTML(paste0("<pre>", pdf_content1$text, "</pre>"))
-        }
-      })
-      
-      output$textOut2 <- renderUI({
-        if(input$headerTab == "tab1header" && input$bodyTab == "tab2body") {
-          HTML(paste0("<pre>", pdf_content2$text, "</pre>"))
-        }
-      })
-      
-      # output$distPlot <- renderPlot({
-      #   if(input$obs != 0 && input$headerTab == "tab2header"){
-      #     hist(rnorm(input$obs), col = "blue",
-      #          main = paste("Histograma Teste - Observações:", input$obs),
-      #          xlab = "", freq = TRUE)
-      #   }
-      # })
-      # 
-      # output$plot2 <- renderPlot({
-      #   if(input$obs != 0 && input$headerTab == "tab2header"){
-      #    plot(density(rnorm(input$obs)))
-      #   }
-      # })
-      
-      output$plot3 <- renderPlot({
-        data <- data.frame(Label = c("A", "B", "C"), Value = c(30, 40, 20))
-        pie(data$Value, labels = data$Label, col = rainbow(nrow(data)))
-      })
-      
-      output$plot4 <- renderPlot({
-        # barplot(table(sample(letters[1:5], input$obs, replace = TRUE)))
-      })
-      
+        )
       }
-    )
-}
+    })
+    
+    # Renderização do gráfico de barras
+    output$graficoBarra <- renderPlot({
+      dadostub <- dados_tubaroes()
+      
+      dadostub <- subset(dadostub, Ano >= input$intervalo_anos[1] & Ano <= input$intervalo_anos[2])
+      
+      # Filtra os dados com base na escolha do sexo
+      if (input$sexo_escolhido == "Macho") {
+        dadostub <- subset(dadostub, Sexo == "M")
+      } else if (input$sexo_escolhido == "Fêmea") {
+        dadostub <- subset(dadostub, Sexo == "F")
+      }
+      
+      # Cria o gráfico ggplot
+      ggplot(dadostub, aes(x = Tamanho)) +
+        geom_histogram(binwidth = 10, fill = "blue", color = "black") +
+        labs(title = "Distribuição de Tamanhos de Tubarões",
+             x = "Tamanho (cm)",
+             y = "Frequência (%)") +
+        scale_y_continuous(labels = scales::label_percent(scale = 0.01))
+    })
+    
+    # Renderização do gráfico de rosca
+    output$graficoRosca <- renderPlot({
+      dadostub <- dados_tubaroes()
+      
+      dadostub <- subset(dadostub, Ano >= input$intervalo_anos[1] & Ano <= input$intervalo_anos[2])
+      
+      gender <- dadostub %>%
+        count(Sexo)
+      
+      gender <- gender %>%
+        mutate(porc = n / sum(n) * 100)
+      
+      gender$ymax <- cumsum(gender$porc)
+      gender$ymin <- c(0, head(gender$ymax, n=1))
+      
+      ggplot(gender, aes(ymax=ymax, ymin=ymin, xmax = 4, xmin = 3, fill = Sexo)) +
+        geom_rect() +
+        coord_polar(theta = "y") +
+        xlim(c(1,4)) +
+        theme_void()
+    })
+    
+    # Renderização do texto da aba 1
+    output$textOut1 <- renderUI({
+      if(input$headerTab == "tab1header" && input$bodyTab == "tab1body") {
+        HTML(paste0("<pre>", pdf_content1$text, "</pre>"))
+      }
+    })
+    
+    # Renderização do texto da aba 2
+    output$textOut2 <- renderUI({
+      if(input$headerTab == "tab1header" && input$bodyTab == "tab2body") {
+        HTML(paste0("<pre>", pdf_content2$text, "</pre>"))
+      }
+    })
+  }
+)
